63466a64cda1ea1e6235a58d306e693369887be4
ASMAE-BOUT <132204159+ASMAE-BOUT@users.noreply.github.com>
2024-01-16 21:36:29 +0100

    Delete Database/Creation Table Batch File.bat

--- Database/Creation Table Batch File.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-@echo Executing SQL Pharùa Table Creation Scripts
-SQLCMD -s DESKTOP-3RKAS3I -d Pharma Proj -u PharmaLogin -p PharmaApp159 -i "D:\C# Projects\Medina Medix Pharma\Base de Données\SQL Query Pharma.sql"
-PAUSE
